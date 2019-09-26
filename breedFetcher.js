14a93e01751eddfd6ecf78311def859e22439120
Terrence Kuiper <github@terrencekuiper.com>
2019-09-26 15:58:00 -0700

    Refactored

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,20 @@
 const request = require('request');
-const args = process.argv.slice(2);
 
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (error, response, body) => {
-  if (error) {
-    console.log('error:', error); // Print the error if one occurred
-  } else {
-    const data = JSON.parse(body);
-    if (body === '[]') {
-      console.log('No Boueno! Cat not found =>', data);
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName[0]}`, (error, response, body) => {
+    if (error) {
+      callback(error, "This is an error");
     } else {
-      console.log(data[0]);
+      const data = JSON.parse(body);
+      if (body === '[]') {
+        callback(`No Boueno! Cat not found`, null);
+      } else {
+        callback(null, data[0].description);
+      }
     }
-  }
+  });
+};
 
+module.exports = { fetchBreedDescription };
 
-});
